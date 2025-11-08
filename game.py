76fcd46166dd979d0a7585326337811fac155426
HiAbdounour <abdounour.misbah@universite-paris-saclay.fr>
2025-11-08 18:22:16 +0100

    Modify position and size of squares

--- game.py
+++ game.py
@@ -2,6 +2,6 @@ from noormi.gaming import *
 
 def build_level(diff=4):
     for i in range(diff):
-        draw_rectangle(Point(250,80+diff*50),50,50,pygame.Color(60,60,60),1)
+        draw_rectangle(Point(70+i*120,200),100,100,pygame.Color(60,60,60),1)
     return
 
